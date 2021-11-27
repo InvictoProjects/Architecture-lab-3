7ce808d2830e328085ace117ece7ab2fd59a0d91
VladyslavTretiak <54894036+Proxima-C@users.noreply.github.com>
2021-11-27 21:52:04 +0200

    Add connections table creation script

--- db/schema.sql
+++ db/schema.sql
@@ -11,3 +11,11 @@ CREATE TABLE machines
     id SERIAL PRIMARY KEY,
     is_working BOOLEAN NOT NULL
 );
+
+DROP TABLE IF EXISTS connections;
+CREATE TABLE connections
+(
+    machine_id int REFERENCES machines (id),
+    balancer_id int REFERENCES balancers (id),
+    PRIMARY KEY (machine_id, balancer_id)
+);
