dd72c1b818bc74d2b72334b0b843beea181c1e9a
VladyslavTretiak <54894036+Proxima-C@users.noreply.github.com>
2021-11-27 22:05:20 +0200

    Insert demo data for balancer 3

--- db/schema.sql
+++ db/schema.sql
@@ -23,9 +23,12 @@ CREATE TABLE connections
 -- Insert demo data.
 INSERT INTO balancers VALUES (DEFAULT);
 INSERT INTO balancers VALUES (DEFAULT);
+INSERT INTO balancers VALUES (DEFAULT);
 
 INSERT INTO machines (is_working) VALUES (TRUE);
 INSERT INTO machines (is_working) VALUES (TRUE);
+INSERT INTO machines (is_working) VALUES (TRUE);
+INSERT INTO machines (is_working) VALUES (FALSE);
 INSERT INTO machines (is_working) VALUES (TRUE);
 INSERT INTO machines (is_working) VALUES (FALSE);
 INSERT INTO machines (is_working) VALUES (TRUE);
@@ -35,3 +38,5 @@ INSERT INTO connections (machine_id, balancer_id) VALUES (2, 1);
 INSERT INTO connections (machine_id, balancer_id) VALUES (3, 2);
 INSERT INTO connections (machine_id, balancer_id) VALUES (4, 2);
 INSERT INTO connections (machine_id, balancer_id) VALUES (5, 2);
+INSERT INTO connections (machine_id, balancer_id) VALUES (6, 3);
+INSERT INTO connections (machine_id, balancer_id) VALUES (7, 3);
