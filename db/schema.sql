5bf7c64660f4026e5cc61714124a34fc7ef8ebe6
Koroliuk <koroliuk.yaroslav@gmail.com>
2021-11-27 21:56:29 +0200

    Update schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,11 +1,11 @@
 -- Create tables.
-DROP TABLE IF EXISTS balancers;
+DROP TABLE IF EXISTS balancers CASCADE;
 CREATE TABLE balancers
 (
     id SERIAL PRIMARY KEY
 );
 
-DROP TABLE IF EXISTS machines;
+DROP TABLE IF EXISTS machines CASCADE;
 CREATE TABLE machines
 (
     id SERIAL PRIMARY KEY,
@@ -15,7 +15,7 @@ CREATE TABLE machines
 DROP TABLE IF EXISTS connections;
 CREATE TABLE connections
 (
-    machine_id int REFERENCES machines (id),
-    balancer_id int REFERENCES balancers (id),
+    machine_id SERIAL REFERENCES machines (id) ON DELETE CASCADE,
+    balancer_id SERIAL REFERENCES balancers (id) ON DELETE CASCADE,
     PRIMARY KEY (machine_id, balancer_id)
 );
