4b63a4dbe05511b5fb9501279251ff11f2bdd68b
Vlad Golik <moyfolder@gmail.com>
2021-11-30 20:47:54 +0200

    Adding machines list to balancer type

--- server/balancers/data.go
+++ server/balancers/data.go
@@ -5,8 +5,9 @@ import (
 )
 
 type Balancer struct {
-	Id                 int64 `json:"id"`
-	TotalMachinesCount int64 `json:"totalMachinesCount"`
+	Id                 int64   `json:"id"`
+	UsedMachines       []int64 `json:"used_machines"`
+	TotalMachinesCount int64   `json:"totalMachinesCount"`
 }
 
 type Store struct {
@@ -31,10 +32,38 @@ func (s *Store) ListBalancers() ([]*Balancer, error) {
 		if err := rows.Scan(&b.Id, &b.TotalMachinesCount); err != nil {
 			return nil, err
 		}
+		machines, err := s.getBalancerMachines(b.Id)
+		if err != nil {
+			return nil, err
+		}
+		b.UsedMachines = machines
 		res = append(res, &b)
 	}
 	if res == nil {
 		res = make([]*Balancer, 0)
 	}
 	return res, nil
+}
+
+func (s *Store) getBalancerMachines(id int64) ([]int64, error) {
+	rows, err := s.Db.Query("SELECT machine_id FROM connections c JOIN machines m on c.machine_id = m.id where balancer_id = $1 AND m.is_working = true;", id)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var machines []int64
+	for rows.Next() {
+		var m int64
+		if err := rows.Scan(&m); err != nil {
+			return nil, err
+		}
+
+		machines = append(machines, m)
+	}
+	if machines == nil {
+		machines = make([]int64, 0)
+	}
+
+	return machines, nil
 }
