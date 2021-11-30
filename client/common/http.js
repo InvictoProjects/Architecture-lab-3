2a44e66a89e1a511329062930eb32676e1e78ac9
Pienskoi <vladymyrpenskoy@gmail.com>
2021-12-01 00:28:21 +0200

    Refactor common/http.js

--- client/common/http.js
+++ client/common/http.js
@@ -1,77 +1,78 @@
-const http = require("http");
+const http = require('http');
 
-const Client = baseUrl => {
-    return {
-        get: path => new Promise((resolve, reject) => {
-            http.get(baseUrl + path, res => {
-                const statusCode = res.statusCode;
-                const contentType = res.headers['content-type'];
+const request = (url, options, callback) => http.request(url, options, res => {
+    const statusCode = res.statusCode;
+    const contentType = res.headers['content-type'];
 
-                let error;
-                if (statusCode !== 200) {
-                    error = new Error('Request Failed.\n' +
-                        `Status Code: ${statusCode}`);
-                } else if (!/^application\/json/.test(contentType)) {
-                    error = new Error('Invalid content-type.\n' +
-                        `Expected application/json but received ${contentType}`);
-                }
-                if (error) {
-                    reject(error);
-                    res.resume();
-                }
-
-                res.setEncoding('utf8');
-                let rawData = '';
-                res.on('data', chunk => {
-                    rawData += chunk;
-                });
-                res.on('end', () => {
-                    try {
-                        const parsedData = JSON.parse(rawData);
-                        resolve(parsedData);
-                    } catch (e) {
-                        reject(e);
-                    }
-                });
-            }).on('error', e => {
-                reject(e);
-            });
-        }),
-        put: async (path, data) => new Promise((resolve, reject) => {
-            const putData = JSON.stringify(data);
-
-            const options = {
-                method: 'PUT',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Content-Length': Buffer.byteLength(putData)
-                }
-            };
-
-            const req = http.request(baseUrl + path, options, res => {
-                res.setEncoding('utf8');
-                let rawData = '';
-                res.on('data', chunk => {
-                    rawData += chunk;
-                });
-                res.on('end', () => {
-                    try {
-                        const parsedData = JSON.parse(rawData);
-                        resolve(parsedData);
-                    } catch (e) {
-                        reject(e);
-                    }
-                });
-            })
-
-            req.on('error', e => {
-                reject(e);
-            });
-
-            req.write(putData);
-            req.end();
-        })
+    let error;
+    if (statusCode !== 200) {
+        error = new Error('Request Failed.\n' +
+            `Status Code: ${statusCode}`);
+    } else if (!/^application\/json/.test(contentType)) {
+        error = new Error('Invalid content-type.\n' +
+            `Expected application/json but received ${contentType}`);
     }
-};
+    if (error) {
+        callback(error, null);
+        res.resume();
+    }
 
-module.exports = {Client};
+    res.setEncoding('utf8');
+    let rawData = '';
+    res.on('data', chunk => {
+        rawData += chunk;
+    });
+    res.on('end', () => callback(rawData));
+});
+
+const get = url => new Promise((resolve, reject) => {
+    const req = request(url, { method: 'GET' }, data => {
+        try {
+            const parsedData = JSON.parse(data);
+            resolve(parsedData);
+        } catch (e) {
+            reject(e);
+        }
+    });
+
+    req.on('error', e => {
+        reject(e);
+    });
+
+    req.end();
+});
+
+const put = (url, data) => new Promise((resolve, reject) => {
+    const putData = JSON.stringify(data);
+
+    const options = {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+            'Content-Length': Buffer.byteLength(putData)
+        }
+    };
+
+    const req = request(url, options, data => {
+        try {
+            const parsedData = JSON.parse(data);
+            resolve(parsedData);
+        } catch (e) {
+            reject(e);
+        }
+    });
+
+    req.on('error', e => {
+        reject(e);
+    });
+
+    req.write(putData);
+    req.end();
+});
+
+const Client = baseUrl => ({
+    get: path => get(baseUrl + path),
+    put: (path, data) => put(baseUrl + path, data)
+});
+
+module.exports = { Client };
