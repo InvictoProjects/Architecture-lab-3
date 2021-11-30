ddd5bf5a98cf3ea14fcd61bf403bbe898d35bbfa
VladyslavTretiak <54894036+Proxima-C@users.noreply.github.com>
2021-11-30 22:01:25 +0200

    Update console log in examples.js

--- client/examples.js
+++ client/examples.js
@@ -9,7 +9,7 @@ balancersClient.listBalancers()
     .then((list) => {
         console.log('=== Scenario 1 ===');
         console.log('List of balancers:');
-        list.forEach((c) => console.log(c.name));
+        list.forEach((c) => console.log(`Id: ${c.Id}, Used Machines: [${c.UsedMachines}], Total Machines Count: ${c.TotalMachinesCount}`));
     })
     .catch((e) => {
         console.log(`Problem listing list of balancers: ${e.message}`);
